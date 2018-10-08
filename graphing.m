6916fa716dcbedac769794cb5a4935529a2c5945
Anthony Androulakis <43938491+AnthonyAndroulakis@users.noreply.github.com>
2018-10-07 22:12:48 -0400

    Update graphing.m

--- graphing.m
+++ graphing.m
@@ -3,9 +3,10 @@ plot(1:length(newsong),newsong*1.5)
 %plot(1:length(newsong),newsong*1.5)
 hold on
 plot(x,y, 'LineWidth', 3)
-title([sprintf('Graph of Signal in Decibels and Treetop Envelope of Signal for Patient ') filename(end-12:end-4)],'Interpreter', 'none');
+locationofslash=strfind(filename,'/'); % if you are using Windows, change this to: locationofslash=strfind(filename,'\');
+title([sprintf('Graph of Signal in Decibels and Treetop Envelope of Signal for Patient ') filename(locationofslash(end)+1:end-4)],'Interpreter', 'none');
 xlabel('Frames (44100 frames = 1 second)') % x-axis label
 ylabel('Decibels') % y-axis label
 legend('Signal (decibels)','Treetop Envelope')
-saveas(gcf,'papergraph3.png')
+saveas(gcf,[filename(locationofslash(end)+1:end-4) '.png'])
 
